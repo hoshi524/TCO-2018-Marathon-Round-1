e7fac48fc417c67fbf313208e7383178ae81aa8d
hoshi524 <hoshi524@gmail.com>
2018-05-13 20:06:48 +0900

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#include <sys/time.h>
 using namespace std;
 
 double dist(double x1, double y1, double x2, double y2) {
@@ -214,6 +213,118 @@ class RoadsAndJunctions {
             fermatPoint(*n.edge[0], *n.edge[1], *n.edge[2], n);
             pos[i - NC][0] = n.x;
             pos[i - NC][1] = n.y;
+          }
+        }
+      }
+    }
+    {
+      init();
+      constexpr int MAX = 6;
+      double prob[MAX + 1][MAX + 1];
+      {
+        memset(prob, 0, sizeof(prob));
+        prob[0][0] = 1;
+        for (int i = 0; i < MAX; ++i) {
+          for (int j = 0; j < MAX; ++j) {
+            prob[i + 1][j] += prob[i][j] * failureProbability;
+            prob[i + 1][j + 1] += prob[i][j] * (1 - failureProbability);
+          }
+        }
+      }
+      int comb[MAX + 1][MAX + 1];
+      {
+        memset(comb, 0, sizeof(comb));
+        for (int i = 0; i <= MAX; ++i) {
+          comb[i][0] = 1;
+          if (i == 0) continue;
+          for (int j = 1; j <= i; ++j) {
+            comb[i][j] = comb[i - 1][j - 1] + comb[i - 1][j];
+          }
+        }
+      }
+      constexpr int SIZE = 1 << 10;
+      constexpr int dx[] = {0, 1, 0, -1};
+      constexpr int dy[] = {1, 0, -1, 0};
+      bool used[SIZE][SIZE];
+      memset(used, false, sizeof(used));
+      int PN = N;
+      for (int i = NC; i < PN; ++i) {
+        vector<int> nl;
+        Node& n = node[i];
+        assert(n.size == 3);
+        auto add = [&](int a, int b) {
+          for (int i = 0; i < 4; ++i) {
+            int x = a + dx[i];
+            int y = b + dy[i];
+            if (x < 0 || SIZE <= x) continue;
+            if (y < 0 || SIZE <= y) continue;
+            if (used[x][y]) continue;
+            used[x][y] = true;
+            nl.push_back((x << 16) | y);
+          }
+        };
+        double d0;
+        {
+          double a = dist(*n.edge[0], *n.edge[1]);
+          double b = dist(*n.edge[1], *n.edge[2]);
+          double c = dist(*n.edge[2], *n.edge[0]);
+          d0 = a + b + c - max({a, b, c});
+        }
+        add(n.x, n.y);
+        auto exp = [&](vector<int>& v) {
+          int s = v.size();
+          double x = junctionCost * v.size();
+          for (int i = 0; i < (1 << s); ++i) {
+            int b = bitset<MAX>(i).count();
+            static double D[3];
+            for (int j = 0; j < 3; ++j) D[j] = 1e10;
+            for (int j = 0; j < s; ++j) {
+              if ((i & (1 << j)) == 0) continue;
+              int x = v[j] >> 16;
+              int y = v[j] & ((1 << 16) - 1);
+              for (int k = 0; k < 3; ++k) {
+                double d = dist(x, y, n.edge[k]->x, n.edge[k]->y);
+                if (D[k] > d) {
+                  D[k] = d;
+                }
+              }
+            }
+            double d = i == 0 ? d0 : (b - 1) + D[0] + D[1] + D[2];
+            x += d * prob[s][b] / comb[s][b];
+          }
+          return x;
+        };
+        vector<int> cl{(n.x << 16) | n.y};
+        double e = exp(cl);
+        for (int m = 1; m < MAX; ++m) {
+          int p = -1;
+          double v = 1e10;
+          for (int t : nl) {
+            int x = t >> 16;
+            int y = t & ((1 << 16) - 1);
+            double d = 0;
+            for (int i = 0; i < n.size; ++i) {
+              d += dist(x, y, n.edge[i]->x, n.edge[i]->y);
+            }
+            if (v > d) {
+              v = d;
+              p = t;
+            }
+          }
+          if (p == -1) break;
+          cl.push_back(p);
+          double ne = exp(cl);
+          if (e > ne) {
+            e = ne;
+            int x = p >> 16;
+            int y = p & ((1 << 16) - 1);
+            nl.erase(remove(nl.begin(), nl.end(), p), nl.end());
+            add(x, y);
+            pos[ps][0] = x;
+            pos[ps][1] = y;
+            ++ps;
+          } else {
+            break;
           }
         }
       }
